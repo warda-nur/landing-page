44d119482e8d1016800e184be9cee94609ad3d74
Warda Nur <wardanur04@gmail.com>
2021-11-12 19:59:14 +0800

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+//document.getElementById("discount").innerHTML = "40% OFF SALE!";
+document.getElementsByTagName("tittle")[0].textContent = "SHOP";
+
+//document.getElementsByTagName("tittle")[0];
+//id.getElementsByTagName("id")[0].innerHTML = "SHOP";
+
+document.getElementById("header");
+
+header.style.backgroundColor = "blue";
